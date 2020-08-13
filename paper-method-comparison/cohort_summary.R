6fc3a1a8a4248ef70450d616c94ad65c34cd1276
Danielle Peterson <dpeterso@wellesley.edu>
2020-08-13 13:31:54 -0400

    phylo tree completed

--- paper-method-comparison/cohort_summary.R
+++ paper-method-comparison/cohort_summary.R
@@ -21,20 +21,8 @@ df$dev_stage <- factor(df$dev_stage,
 # age statistics
 
 df_stage1 <- df[df$dev_stage == "less than 15 months",]
-#mean(df_stage1[df_stage1$method == "mgx", ]$shannon)
-#mean(df_stage1[df_stage1$method == "amp", ]$shannon)
-#t.test(as.numeric(df_stage1$shannon)~ df_stage1$method)
-
 df_stage2 <- df[df$dev_stage == "15 to 30 months",]
-#kruskal.test(as.numeric(df_stage2$shannon) ~ df_stage2$method)
-
 df_stage3 <- df[df$dev_stage == "older than 30 months",]
-#kruskal.test(as.numeric(df_stage3$shannon) ~ df_stage3$method)
-
-#anova_dev_stage <- aov(as.numeric(df$shannon) ~ df$dev_stage)
-#summary(anova_dev_stage)
-#posthoc <- TukeyHSD(anova_dev_stage, 'df$dev_stage', conf.level=0.95)
-#posthoc
 
 # number of kids in each age group
 
@@ -56,13 +44,6 @@ mean(df_stage3$AgeMonths)
 sqrt(var(df_stage3$AgeMonths))
 
 # only counting 1 profiling method (to get number of kids, not profiles)
-#df_stage1_mgx <- df_stage1[df_stage1$method == "mgx",]
-#df_stage2_mgx <- df_stage2[df_stage2$method == "mgx",]
-#df_stage3_mgx <- df_stage3[df_stage3$method == "mgx",]
-
-#df_ages <- df[,c("sampleid","method","dev_stage", "AgeMonths")]
-#df_ages <- df_ages[df_ages$method == "mgx",]
-
 
 ageplot<- ggplot(df, aes(x = AgeMonths)) + 
   geom_histogram(data=df_stage1, fill = "#481567FF", alpha=0.7, binwidth = 10)+
